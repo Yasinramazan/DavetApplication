70a1df03c772dce6a1c481cabb07d052c8e223dd
Yasinramazan <yasin0496@gmail.com>
2024-09-02 00:46:49 +0300

    Right Bar Completed Now

--- Components/discovercard/discovercard.tsx
+++ Components/discovercard/discovercard.tsx
@@ -1,9 +1,33 @@
 import styles from "../discovercard/discovercard.module.css"
+import DiscoverContent from "../discovercontent/discovercontent";
 
-export default function DiscoverCard(){
-    return(
+export default function DiscoverCard() {
+
+    const people = [
+        { name: 'Ali', age: "30", occupation: 'Mühendis' },
+        { name: 'Ayşe', age: "25", occupation: 'Doktor' },
+        { name: 'Ahmet', age: "40", occupation: 'Öğretmen' },
+        { name: 'Fatma', age: "35", occupation: 'Hemşire' },
+        { name: 'Mehmet', age: "50", occupation: 'Yazılımcı' },
+        { name: 'Elif', age: "28", occupation: 'Grafik Tasarımcı' },
+        { name: 'Ahmet', age: "45", occupation: 'Müdür' },
+        { name: 'Zeynep', age: "32", occupation: 'Psikolog' },
+        { name: 'Oğuz', age: "27", occupation: 'Avukat' },
+        { name: 'Deniz', age: "22", occupation: 'Stajyer' }
+    ];
+
+    return (
         <div className={styles.outerdiv}>
             <h2 className={styles.h2}>İlgini çekebilecek gündemler</h2>
+            <div className={styles.outernewsdiv} >
+                {people.map((item,index)=>
+                <div key={index}>
+                    <DiscoverContent title={item.occupation} name={item.name} total={item.age}/>
+                </div>
+                    
+                )}
+                
+            </div>
         </div>
     );
 }
